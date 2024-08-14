4bcd4cf2033a3091d19343101d9dbcf38be0bea1
master.cokorda@gmail.com <master.cokorda@gmail.com>
2024-08-14 21:24:16 +0800

    2124

---
+++ index.js
@@ -0,0 +1,7 @@
+const tomboldarkmode = document.getElementById("modegelap");
+const navbar = document.getElementById("navbar");
+const body = document.body;
+function klikmodegelap() {
+  body.classList.toggle("styledarkmode");
+  navbar.classList.toggle("styledarkmode");
+}
