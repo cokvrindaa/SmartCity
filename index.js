930e93a38d806acfeaf4dcb2b394f77fffd84caa
master.cokorda@gmail.com <master.cokorda@gmail.com>
2024-08-15 16:13:00 +0800

    dropdown

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tomboldarkmode = document.getElementById("modegelap");
 const navbar = document.getElementById("navbar");
 const body = document.body;
+const menu = document.getElementById("menu");
+const dropdown = document.getElementById("dropdown");
 function klikmodegelap() {
   body.classList.add("styledarkmode");
   navbar.classList.add("styledarkmode");
@@ -9,3 +11,7 @@ function klikmodeterang() {
   body.classList.remove("styledarkmode");
   navbar.classList.remove("styledarkmode");
 }
+
+menu.addEventListener("click", function(){
+  dropdown.style.display = "block"
+})
