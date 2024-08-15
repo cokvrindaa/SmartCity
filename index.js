374bb56dacfb3a12cacb4a2ca4aacae01559164a
cokvrindaa <master.cokorda@gmail.com>
2024-08-15 18:01:31 +0800

    1801

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ const navbar = document.getElementById("navbar");
 const body = document.body;
 const menu = document.getElementById("menu");
 const dropdown = document.getElementById("dropdown");
+const linkkotaka = document.getElementById("kotaklinka");
+const linkkotakb = document.getElementById("kotaklinkb");
+const linkkotakc = document.getElementById("kotaklinkc");
+
 function klikmodegelap() {
   body.classList.add("styledarkmode");
   navbar.classList.add("styledarkmode");
+  dropdown.classList.add("styledarkmode")
+  dropdown.classList.add("styledarkmode")
+  linkkotaka.classList.add("styledarkmode")
+  linkkotakb.classList.add("styledarkmode")
+  linkkotakc.classList.add("styledarkmode")
+
 }
 function klikmodeterang() {
   body.classList.remove("styledarkmode");
   navbar.classList.remove("styledarkmode");
+  dropdown.classList.remove("styledarkmode")
+  linkkotaka.classList.remove("styledarkmode")
+  linkkotakb.classList.remove("styledarkmode")
+  linkkotakc.classList.remove("styledarkmode")
 }
-
 menu.addEventListener("click", function(){
   if (dropdown.style.display === 'none' ) {
     dropdown.style.display = 'block';
